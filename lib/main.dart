8612ca8753520beb40d1a06e8301909be5d9bcbb
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-09-15 09:20:44 -0300

    feat: setup inicial

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:organize/src/organize.dart';
+
+void main() {
+  runApp(Organize());
+}
---
+++ lib/src/organize.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Organize extends StatelessWidget {
+  const Organize({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
