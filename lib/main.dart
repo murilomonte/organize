abcee3150149aece55e8f22307977b9ed491cd7c
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-09-23 09:18:12 -0300

    feat: viewmodel partially implemented

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:organize/src/data/database/app_database.dart';
 import 'package:organize/src/data/repositories/organize_repository.dart';
 import 'package:organize/src/organize.dart';
+import 'package:organize/src/view_models/group_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +13,15 @@ void main() async {
     groupsDao: db.groupsDao,
     tasksDao: db.tasksDao,
     subtasksDao: db.subtasksDao,
-    statusDao: db.statusDao
+    statusDao: db.statusDao,
   );
 
-  runApp(Organize());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => GroupViewModel(repo: repo)),
+      ],
+      child: Organize(),
+    ),
+  );
 }
--- lib/src/view_models/group_view_model.dart
+++ lib/src/view_models/group_view_model.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:organize/src/data/repositories/organize_repository.dart';
+import 'package:organize/src/models/group_model.dart';
 
 class GroupViewModel extends ChangeNotifier {
-  final OrganizeRepository repository;
+  final OrganizeRepository repo;
 
-  GroupViewModel({
-    required this.repository
-  });
+  List<GroupModel> groupList = [];
+  String errorMsg = '';
+  bool isLoading = false;
 
+  GroupViewModel({required this.repo}) {
+    _updateGroupList();
+  }
+
+  void _updateGroupList() async {
+    isLoading = true;
+    notifyListeners();
+
+    final result = await repo.getAllGroupWithTasksAndSubtasks();
+    switch (result) {
+      case Success(data: List<GroupModel> list):
+        groupList = list;
+        break;
+
+      case Failure(message: String message):
+        errorMsg = message;
+        break;
+    }
+    isLoading = false;
+    notifyListeners();
+  }
 }
